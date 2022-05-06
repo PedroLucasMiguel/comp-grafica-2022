3b9787711b94dd19603989ab5014ba370d08344e
Predo <pedro.l.miguel@unesp.br>
2022-05-06 13:55:32 -0300

    Finally some good fucking images :D
    Now we can generate the images

--- code/src/image-stuff.py
+++ code/src/image-stuff.py
@@ -2,33 +2,37 @@ import numpy
 from PIL import Image
 import numpy as np
 
-START_COLOR = 180
 
-
-def createimage(size, squares_per_row):
+def createimage(size, squares_per_row, square_start_color, color_increment_offset):
     i = numpy.array([])
+    current_color = square_start_color
+    total_squares = squares_per_row * squares_per_row
+    gradient = []
+    gradient_index = 0
+    gradient_offset = 0
+
+    for a in range(total_squares):
+        current_color = current_color + color_increment_offset
+        gradient.append(current_color)
+        print(gradient[a])
 
     if squares_per_row != 1:
-        square_size = size / squares_per_row
+        square_size = int(size / squares_per_row)
 
-        for x in range(size):
-            if x < square_size - 1:
-                for y in range(size):
-                    if y < square_size:
-                        i = np.append(i, START_COLOR)
-                    else:
-                        i = np.append(i, START_COLOR + 10)
-            else:
-                for y in range(size):
-                    if y < square_size:
-                        i = np.append(i, START_COLOR + 20)
-                    else:
-                        i = np.append(i, START_COLOR + 30)
+        for s in range(squares_per_row):
+            for k in range(square_size):
+                for y in range(squares_per_row):
+                    for x in range(square_size):
+                        i = np.append(i, gradient[gradient_index])
+                    gradient_index = gradient_index + 1
+                gradient_index = gradient_offset
+            gradient_offset = gradient_offset + squares_per_row - 1
+
 
     else:
         for x in range(size):
             for y in range(size):
-                i = np.append(i, START_COLOR)
+                i = np.append(i, current_color)
 
     i = np.reshape(i, (size, size))
     i = Image.fromarray(i)
@@ -37,4 +41,5 @@ def createimage(size, squares_per_row):
 
 
 if __name__ == '__main__':
-        createimage(256, 2)
+        #createimage(256, 1, 150)
+    createimage(256, 4, 120, 10)
