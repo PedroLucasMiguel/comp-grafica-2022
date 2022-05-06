9d34477dfd5125cfc5b535d799aa26b2e3dc2697
Predo <pedro.l.miguel@unesp.br>
2022-05-06 19:10:58 -0300

    Making the code a liitle bit prettier :^)
    Just making some changes in order to make the code
    a little bit more readable.

--- code/src/image-stuff.py
+++ code/src/image-stuff.py
@@ -3,43 +3,45 @@ from PIL import Image
 import numpy as np
 
 
-def createimage(size, squares_per_row, square_start_color, color_increment_offset):
+def createimage(size, squares_per_row, square_start_color, color_increment):
     i = numpy.array([])
-    current_color = square_start_color
     total_squares = squares_per_row * squares_per_row
     gradient = []
     gradient_index = 0
     gradient_offset = 0
 
-    for a in range(total_squares):
-        current_color = current_color + color_increment_offset
-        gradient.append(current_color)
-        print(gradient[a])
-
     if squares_per_row != 1:
         square_size = int(size / squares_per_row)
 
+        aux_color = square_start_color
+
+        # Creating the color gradient
+        for a in range(total_squares):
+            aux_color = aux_color + color_increment
+            gradient.append(aux_color)
+
+        # Creating the image on the array
         for s in range(squares_per_row):
-            for k in range(square_size):
-                for y in range(squares_per_row):
+            for y in range(square_size):
+                for y_row in range(squares_per_row):
                     for x in range(square_size):
                         i = np.append(i, gradient[gradient_index])
                     gradient_index = gradient_index + 1
                 gradient_index = gradient_offset
             gradient_offset = gradient_offset + squares_per_row - 1
 
-
     else:
         for x in range(size):
             for y in range(size):
-                i = np.append(i, current_color)
+                i = np.append(i, square_start_color)
 
     i = np.reshape(i, (size, size))
-    i = Image.fromarray(i)
-    i = i.convert('L')
-    i.save('teste.bmp', 'bmp')
+    pil_image = Image.fromarray(i)
+    pil_image = pil_image.convert('L')
+    pil_image.save(f'{squares_per_row}.bmp', 'bmp')
+
+    return i
 
 
 if __name__ == '__main__':
-        #createimage(256, 1, 150)
-    createimage(256, 4, 120, 10)
+    createimage(size=256, squares_per_row=2, square_start_color=180, color_increment=10)
