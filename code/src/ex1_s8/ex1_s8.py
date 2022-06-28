0f5c505b755c81d45e1ed804e6489dc4e3f683f8
PedroLucasMiguel <pedro.lu.miguel@gmail.com>
2022-06-28 10:42:06 -0300

    Implementing (Something) that looks like multi-class Otsu
    This definitly is not perfect, and it's really bad.
    
    I'll try to make the function a little bit prettier another time

--- code/src/ex1_s8/ex1_s8.py
+++ code/src/ex1_s8/ex1_s8.py
@@ -1,7 +1,10 @@
+from turtle import right
 import cv2
 import numpy as np
 from os import path
 from matplotlib import pyplot as plt
+from math import floor
+from numba import njit
 
 def __do_one_Otsu(img):
 
@@ -23,7 +26,6 @@ def __do_one_Otsu(img):
     for i in range(l+1):
         hist[i][1] = hist[i][1]/qtd_pixels
 
-
     v_final = -1
     k_final = 0
 
@@ -40,8 +42,6 @@ def __do_one_Otsu(img):
 
         if p1 < 1.e-6 or p2 < 1.e-6:
             continue
-
-        #print(f'P1: {p1} || P2: {p2}')
 
         # Calculando m1
         m1 = 0
@@ -74,8 +74,8 @@ def __do_one_Otsu(img):
             
     cv2.imwrite(path.join('src', 'output', f'a_plz.png'), img)
 
+@njit
 def __doOtsu(img):
-
     l = np.amax(img)
     qtd_pixels = img.shape[0] * img.shape[1]
     
@@ -83,7 +83,7 @@ def __doOtsu(img):
 
     # Criando a tabela base do histograma
     for i in range(l+1):
-        hist.insert(i, [i, 0])
+        hist.insert(i, [i, 0.0])
 
     # Preenchendo tabela base
     for i in img:
@@ -92,24 +92,25 @@ def __doOtsu(img):
 
     # Normalizando histograma
     for i in range(l+1):
-        hist[i][1] = hist[i][1]/qtd_pixels
-
-    # ------------------- Otsu --------------------------
+        hist[i][1] = float(hist[i][1]/qtd_pixels)
+    
+    # -------------------------------------------------------------
     v_final = -1
-    k_f = [] # tresholds
+    k_fe = []
 
     # Começando o processo de Otsu para duas classes
-    # FIX-ME: Expandir para 4 classes
-    for k1 in range(0, l):
+    for k1 in range(0, 63):
         print(k1)
         p1 = 0
         for i in range(0, k1 + 1):
             p1 = p1 + (hist[i][1])
 
         # Calculando P2
-        p2 = 1 - p1
-        
-        if p1 < 1.e-6 or p2 < 1.e-6: # Checagem para evitar divisão por 0
+        p2 = 0
+        for i in range(k1 + 1, 63):
+            p2 = p2 + (hist[i][1])
+
+        if p1 < 1.e-6 or p2 < 1.e-6:
             continue
 
         # Calculando m1
@@ -122,24 +123,21 @@ def __doOtsu(img):
         for i in range(k1 + 1, l):
             m2 = m2 + ((i * hist[i][1]) / p2)
 
-        # Calculando para o 2° treshold
-        for k2 in range(k1, l):
-            #print('b')
-            p3 = 1 - p2
-
+        for k2 in range(k1 + 1, 126):
+            p3 = 0
+            for i in range(k2 + 1, 126):
+                p3 = p3 + (hist[i][1])
             if p3 < 1.e-6:
-                print('fuck....')
                 continue
 
             m3 = 0
             for i in range(k2 + 1, l):
                 m3 = m3 + ((i * hist[i][1]) / p3)
-
-            # Calculando para o 3° treshold
-            for k3 in range(k2, l):
-                #print('c')
-                p4 = 1 - p3
-
+            
+            for k3 in range(k2+1, 189):
+                p4 = 0
+                for i in range(k3 + 1, 189):
+                    p4 = p4 + (hist[i][1])
                 if p4 < 1.e-6:
                     continue
 
@@ -147,15 +145,17 @@ def __doOtsu(img):
                 for i in range(k3 + 1, l):
                     m4 = m4 + ((i * hist[i][1]) / p4)
 
-                # Calculando para o 4° treshold
-                for k4 in range(k3, l):
-                    p5 = 1 - p4
+                for k4 in range(k3+1, 256):
+                    
+                    p5 = 0
+                    for i in range(k4 + 1, 256):
+                        p5 = p5 + (hist[i][1])
 
                     if p5 < 1.e-6:
                         continue
 
                     m5 = 0
-                    for i in range(k4 + 1, l):
+                    for i in range(k4 + 1, 256):
                         m5 = m5 + ((i * hist[i][1]) / p5)
 
                     # Calculando mt
@@ -163,32 +163,82 @@ def __doOtsu(img):
 
                     # Calculando variância
                     v = (p1*((m1-mt)**2)) + (p2*((m2-mt)**2)) + (p3*((m3-mt)**2)) + (p4*((m4-mt)**2)) + (p5*((m5-mt)**2))
-                    #print(v)
+                
                     if v > v_final:
-                        k_f.insert(0, k1)
-                        k_f.insert(1, k2)
-                        k_f.insert(2, k3)
-                        k_f.insert(3, k4)
+                        if len(k_fe) == 0:
+                            v_final = v
+                            k_fe.append(k1)
+                            k_fe.append(k2)
+                            k_fe.append(k3)
+                            k_fe.append(k4)
+                    
+                        else:
+                            k_fe[0] = k1
+                            k_fe[1] = k2
+                            k_fe[2] = k3
+                            k_fe[3] = k4
+    
+    print(v, k_fe)
 
-    print(v_final, k_f)
-    '''
     for i in range(img.shape[0]):
         for j in range(img.shape[1]):
-            if img[i][j] > k_final:
+            if img[i][j] > k_fe[3]:
                 img[i][j] = 255
-            else:
+            elif img[i][j] <= k_fe[0] :
                 img[i][j] = 0
+            elif img[i][j] > k_fe[0]  and img[i][j] <= k_fe[1] :
+                img[i][j] = 60
+            elif img[i][j] > k_fe[1]  and img[i][j] <= k_fe[2] :
+                img[i][j] = 130
+            elif img[i][j] > k_fe[2]  and img[i][j] <= k_fe[3] :
+                img[i][j] = 180
+
+
     '''
+    img1 = np.zeros(shape=img.shape)
+    for i in range(img.shape[0]):
+        for j in range(img.shape[1]):
+            if img[i][j] > k_fe[0]:
+                img1[i][j] = 255
+            else:
+                img1[i][j] = 0
+
+    img2 = np.zeros(shape=img.shape)
+    for i in range(img.shape[0]):
+        for j in range(img.shape[1]):
+            if img[i][j] > k_fe[1]:
+                img2[i][j] = 255
+            else:
+                img2[i][j] = 0
+
+    img3 = np.zeros(shape=img.shape)
+    for i in range(img.shape[0]):
+        for j in range(img.shape[1]):
+            if img[i][j] > k_fe[2]:
+                img3[i][j] = 255
+            else:
+                img3[i][j] = 0
+
+    img4 = np.zeros(shape=img.shape)
+    for i in range(img.shape[0]):
+        for j in range(img.shape[1]):
+            if img[i][j] > k_fe[3]:
+                img4[i][j] = 255
+            else:
+                img4[i][j] = 0
     
-    cv2.imwrite(path.join('src', 'output', f'a_plz.png'), img)
+    return (img1, img2, img3, img4)
+    '''
 
-
+    return img
+    
 if __name__ == '__main__':
     
-    img = cv2.imread(path.join('src', 'output', 'help_me.png'), cv2.IMREAD_GRAYSCALE)
+    img = cv2.imread(path.join('src', 'images', 'img_seg.jpg'), cv2.IMREAD_GRAYSCALE)
 
-    __doOtsu(img)
-
-    #cv2.imwrite(path.join('src', 'output', 'img_seg_r.png'), img)
-
-    #cv2.imwrite(path.join('src', 'output', 'img_seg_gs.png'), img)
+    cv2.imwrite(path.join('src', 'output', f'a_plz.png'), cv2.applyColorMap(__doOtsu(img), cv2.COLORMAP_JET) )
+    #imgs = __doOtsu(img)
+    #cv2.imwrite(path.join('src', 'output', f'1.png'), imgs[0])
+    #cv2.imwrite(path.join('src', 'output', f'2.png'), imgs[1])
+    #cv2.imwrite(path.join('src', 'output', f'3.png'), imgs[2])
+    #cv2.imwrite(path.join('src', 'output', f'4.png'), imgs[3])
